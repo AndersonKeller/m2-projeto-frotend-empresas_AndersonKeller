4885305bee8c19beca996feb0e5412c205446e8a
Anderson Keller <andersonkellervendas@hotmail.com>
2022-10-31 22:00:42 -0300

    feat/register.js

---
+++ pages/register/index.js
@@ -0,0 +1,22 @@
+import { registerUser } from "../../scripts/requests.js";
+
+
+function registerUserForm(){
+    const formRegister = document.querySelector("form");
+    const list = [...formRegister.elements];
+    const user ={}
+    formRegister.addEventListener("submit",async (e)=>{
+        e.preventDefault()
+        console.log("oi")
+        list.forEach((element)=>{
+            
+           if(element.tagName == "INPUT" || element.tagName == "SELECT"){
+
+                user[element.id] = element.value
+           }
+        })
+        const resp = await registerUser(user)
+        console.log(resp)
+    })
+}
+registerUserForm()
