965489732903dc51035fe42b16b0bb089265c7c7
Anderson Keller <andersonkellervendas@hotmail.com>
2022-11-02 19:35:51 -0300

    feat:modalSelect

--- scripts/forms.js
+++ scripts/forms.js
@@ -1,4 +1,4 @@
-
+import { getAllCompany } from "./requests.js";
 
 export async function createForm(){
     
@@ -8,10 +8,20 @@ export async function createForm(){
     <form>
         <input required id="name" type="text" placeholder="Nome do departamento">
         <input required  id="description" type="text" placeholder="Descrição do departamento">
-        <select name="company_uuid" id="">
+        <select name="company_uuid" id="company-modal">
             <option value="">Selecionar empresa</option>
         </select>
         <button type="submit" class="btn-form text-btn">Criar o departamento</button>
     </form>
     `)
+    const companies = await getAllCompany();
+    console.log(companies)
+    companies.forEach((comp)=>{
+        const select = document.querySelector("#company-modal")
+    console.log(select)
+    select.insertAdjacentHTML("beforeend",`
+    <option value="${comp.uuid}">${comp.name}</option>
+    `)
+    })
+    
 }
