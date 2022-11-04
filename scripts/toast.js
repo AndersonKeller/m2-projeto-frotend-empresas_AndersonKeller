844cab12bc8394a0b1038f820ed96a6b9c5b8905
Anderson Keller <andersonkellervendas@hotmail.com>
2022-11-04 11:27:09 -0300

    create toast login

---
+++ scripts/toast.js
@@ -0,0 +1,16 @@
+export async function toastCreate(status,text){
+    const main = document.querySelector("main");
+    const divMain = document.createElement("div");
+    divMain.classList.add("toast-container");
+    if(status=="ok"){
+        divMain.classList.add("sucess");
+    }else{
+        divMain.classList.add("error")
+    }
+    const h2text = document.createElement("h3");
+    h2text.innerText = text;
+
+    divMain.appendChild(h2text)
+    main.appendChild(divMain)
+   // divMain.remove()
+}
