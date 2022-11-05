6fcf13e4384e6c2787fe398510da3c5d20d08350
Anderson Keller <andersonkellervendas@hotmail.com>
2022-11-04 23:00:10 -0300

    fix menu open register page

--- pages/register/index.js
+++ pages/register/index.js
@@ -20,3 +20,14 @@ function registerUserForm(){
     })
 }
 registerUserForm()
+
+showMenu()
+function showMenu(){
+    const showMenu = document.querySelector(".show-menu");
+    showMenu.addEventListener("click",()=>{
+        const divBtns = document.querySelector(".nav-btns");
+        console.log("Oi")
+        divBtns.classList.toggle("show-div-btns");
+        showMenu.classList.toggle("close-menu");
+    })
+}
